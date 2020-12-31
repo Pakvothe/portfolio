408456ed27a4aaa5a49eccec02aba17ff55a129c
Pakvothe <franqitto@gmail.com>
2020-12-31 04:03:36 -0300

    fixed year of production date

--- src/components/footer/strings.js
+++ src/components/footer/strings.js
@@ -1,12 +1,12 @@
 const strings = {
 	en: {
 		a: 'Designed & Developed by',
-		b: 'Franco Ortiz ~ 2020',
+		b: 'Franco Ortiz ~ 2021',
 		c: '->Repository<-',
 	},
 	es: {
 		a: 'Diseñado y Desarrolado por',
-		b: 'Franco Ortiz ~ 2020',
+		b: 'Franco Ortiz ~ 2021',
 		c: '->Repositorio<-',
 	}
 };
