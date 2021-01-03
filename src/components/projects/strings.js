2f9c327fefdc7365b7316d33e6ec60b0f0029cee
Pakvothe <franqitto@gmail.com>
2021-01-03 04:02:03 -0300

    add tetris game project

--- src/components/projects/strings.js
+++ src/components/projects/strings.js
@@ -3,12 +3,13 @@ const strings = {
 		title: 'Projects',
 		repo: 'Repository',
 		fw: 'Simple application made with React, accesses the OpenWeather API and allows to obtain the temperature of any city in the world, has a mobile version. Minimalist aesthetic with careful contrasts.',
+		tetris: 'This is a "Tetris" game built entirely in Javascript with React.js. Styled Components were used to style it. The game is fully functional, it is managed with the keyboard arrows and it allows you to go up in score and difficulty.',
 	},
 	es: {
 		title: 'Proyectos',
 		repo: 'Repositorio',
 		fw: 'Aplicacion simple realizada con React, accede a la OpenWeather API y permite obtener la temperatura de cualquier ciudad del mundo, tiene version mobile. Estetica minimalista con constrastes cuidados.',
-
+		tetris: 'Este es un juego "Tetris" creado por completo en Javascript con  React.js. Se uso Styled Components para darle estilos.  El juego es totalmente funcional,  se maneja con las flechas del teclado y permite ir subiendo de puntaje y dificultad.',
 	}
 };
 
