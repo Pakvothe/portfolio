c550912cfec5606b81b1362728d69fe0fe21cc2c
Pakvothe <franqitto@gmail.com>
2021-02-03 20:35:30 -0300

    fixed ES resume and background strings

--- src/components/background/strings.js
+++ src/components/background/strings.js
@@ -2,7 +2,7 @@ const strings = {
 	en: {
 		title: 'Background',
 		p1:
-			"I am currently a web developer Fullstack (PERN stack) and I am in search of my first job in the IT world. Recently graduated from the bootcamp 'soyHenry' (800hs) after months of dedication.",
+			"I am a fullstack web developer, with preference to Front-End and mobile development. I am looking for a job which allows me to gain experience. Recently graduated from the 'soyHenry' bootcamp (800hs) after months of dedication.",
 		p2:
 			"As a developer I enjoy constantly learn new technologies and improve the already learned. I like the challenges that leave me out of my comfort zone and the satisfaction of achieving them. My passion is to create interfaces polished and beautiful for the end consumer, so I'm a frontend developer oriented.",
 		p3:
@@ -12,7 +12,7 @@ const strings = {
 	es: {
 		title: 'Trasfondo',
 		p1:
-			"Actualmente soy un desarrollador web fullstack (PERN stack) y me encuentro en busqueda de mi primer trabajo en el mundo del IT. Recientemente graduado del bootcamp 'soyHenry' (800hs) despues de meses de dedicación.",
+			"Soy un desarrollador web fullstack, con preferencia al Front-End y al desarrollo mobile. Me encuentro en busqueda de un trabajo el cual me permita adquirir experiencia. Recientemente graduado del bootcamp 'soyHenry' (800hs) despues de meses de dedicación.",
 		p2:
 			'Como desarrollador disfruto de aprender constantemente tecnologias nuevas y perfeccionar las ya aprendidas. Me gustan los desafios que me hacen salir de mi zona de confort y la satisfaccion de concretarlos. Mi pasion es crear interfaces pulidas y bellas para el consumidor final, por lo que soy un desarrollador orientado al frontend.',
 		p3:
