ca5db326eb6e616d08bd4f22cde32da58338fd6a
Pakvothe <fr.dv.ortiz@gmail.com>
2023-02-20 04:42:30 -0300

    remove firebase junk

--- src/components/home_page/index.js
+++ src/components/home_page/index.js
@@ -1,119 +1,89 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setIsOpen } from '../../redux/actions';
-import { getFirestore } from '../../firebase';
-import firebase from 'firebase/app';
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setIsOpen } from "../../redux/actions";
 
 //components ==>
-import Loading from '../loading';
-import Navbar from '../navbar';
-import Intro from '../intro';
-import Background from '../background';
-import Education from '../education';
-import Contact from '../contact';
-import Experience from '../experience';
-import Projects from '../projects';
-import Footer from '../footer';
+import Navbar from "../navbar";
+import Intro from "../intro";
+import Background from "../background";
+import Education from "../education";
+import Contact from "../contact";
+import Experience from "../experience";
+import Projects from "../projects";
+import Footer from "../footer";
 
 //styles ==>
-import arrowUp from '../../assets/img/arrow-up.svg';
-import { StyledSVG, ScrollButton } from '../styles/GlobalStyle';
-import { animateScroll } from 'react-scroll';
+import arrowUp from "../../assets/img/arrow-up.svg";
+import { StyledSVG, ScrollButton } from "../styles/GlobalStyle";
+import { animateScroll } from "react-scroll";
 
 //strings ==>
-import contactStrings from '../contact/strings';
+import contactStrings from "../contact/strings";
 
 const HomePage = () => {
-	const dispatch = useDispatch();
-	const scrollButton = useRef();
-	const contactButton = useRef();
-	const language = useSelector((state) => state.language);
-	const [loading, setLoading] = useState(true);
-	const [visits, setVisits] = useState(0);
+  const dispatch = useDispatch();
+  const scrollButton = useRef();
+  const contactButton = useRef();
+  const language = useSelector((state) => state.language);
 
-	useEffect(() => {
-		const db = getFirestore();
-		const counter = db.collection('Counter').doc('contador666');
-		counter.get().then((visit) => {
-			setLoading(false);
+  window.onscroll = function () {
+    scrollFunction();
+  };
+  const scrollDistance = 700;
 
-			if (!localStorage.getItem('visitante')) {
-				localStorage.setItem('visitante', '1');
-				counter.update({
-					counter: firebase.firestore.FieldValue.increment(1),
-				});
-			}
-			setVisits(visit.data().counter);
-		});
-	}, []);
+  const openModal = () => {
+    dispatch(setIsOpen(true));
+  };
+  function scrollFunction() {
+    if (scrollButton.current) {
+      if (
+        document.body.scrollTop > scrollDistance ||
+        document.documentElement.scrollTop > scrollDistance
+      ) {
+        scrollButton.current.style.pointerEvents = "auto";
+        scrollButton.current.style.opacity = "100";
+      } else {
+        scrollButton.current.style.pointerEvents = "none";
+        scrollButton.current.style.opacity = "0";
+      }
+    }
+    if (contactButton.current) {
+      if (
+        document.body.scrollTop > scrollDistance ||
+        document.documentElement.scrollTop > scrollDistance
+      ) {
+        contactButton.current.style.pointerEvents = "auto";
+        contactButton.current.style.opacity = "100";
+      } else {
+        contactButton.current.style.pointerEvents = "none";
+        contactButton.current.style.opacity = "0";
+      }
+    }
+  }
+  const scrollToTop = () => {
+    animateScroll.scrollToTop();
+  };
 
-	window.onscroll = function () {
-		scrollFunction();
-	};
-	const scrollDistance = 700;
-
-	const openModal = () => {
-		dispatch(setIsOpen(true));
-	};
-	function scrollFunction() {
-		if (scrollButton.current) {
-			if (
-				document.body.scrollTop > scrollDistance ||
-				document.documentElement.scrollTop > scrollDistance
-			) {
-				scrollButton.current.style.pointerEvents = 'auto';
-				scrollButton.current.style.opacity = '100';
-			} else {
-				scrollButton.current.style.pointerEvents = 'none';
-				scrollButton.current.style.opacity = '0';
-			}
-		}
-		if (contactButton.current) {
-			if (
-				document.body.scrollTop > scrollDistance ||
-				document.documentElement.scrollTop > scrollDistance
-			) {
-				contactButton.current.style.pointerEvents = 'auto';
-				contactButton.current.style.opacity = '100';
-			} else {
-				contactButton.current.style.pointerEvents = 'none';
-				contactButton.current.style.opacity = '0';
-			}
-		}
-	}
-	const scrollToTop = () => {
-		animateScroll.scrollToTop();
-	};
-
-	return (
-		<>
-			{loading === false ? (
-				<div>
-					<Navbar visits={visits} />
-					<Intro />
-					<Background />
-					<Education />
-					<Contact />
-					<Experience/>
-					<Projects />
-					<Footer />
-					<ScrollButton ref={scrollButton} onClick={scrollToTop}>
-						<StyledSVG src={arrowUp} />
-					</ScrollButton>
-					<button
-						ref={contactButton}
-						className="contact"
-						onClick={openModal}
-					>
-						<p>{contactStrings[language].contact}</p>
-					</button>
-				</div>
-			) : (
-				<Loading loading={loading} />
-			)}
-		</>
-	);
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Intro />
+        <Background />
+        <Education />
+        <Contact />
+        <Experience />
+        <Projects />
+        <Footer />
+        <ScrollButton ref={scrollButton} onClick={scrollToTop}>
+          <StyledSVG src={arrowUp} />
+        </ScrollButton>
+        <button ref={contactButton} className="contact" onClick={openModal}>
+          <p>{contactStrings[language].contact}</p>
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
-
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,100 +1,75 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 //styles ==>
-import { NavbarStyled, Dropdown, StyledSVG } from '../styles/styled_navbar';
-import Fade from 'react-reveal/Fade';
+import { NavbarStyled, Dropdown, StyledSVG } from "../styles/styled_navbar";
+import Fade from "react-reveal/Fade";
 //actions ==>
-import { toggleTheme, changeLanguage } from '../../redux/actions';
+import { toggleTheme, changeLanguage } from "../../redux/actions";
 
 //logos ==>
-import moon from '../../assets/img/moon.svg';
-import sun from '../../assets/img/sun.svg';
-import languageIcon from '../../assets/img/language.svg';
-import user from '../../assets/img/user.svg';
+import moon from "../../assets/img/moon.svg";
+import sun from "../../assets/img/sun.svg";
+import languageIcon from "../../assets/img/language.svg";
 
 //strings ==>
-import strings from './strings';
+import strings from "./strings";
 
-const Navbar = ({ visits }) => {
-	const dispatch = useDispatch();
-	const language = useSelector((state) => state.language);
-	const theme = useSelector((state) => state.theme);
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const language = useSelector((state) => state.language);
+  const theme = useSelector((state) => state.theme);
 
-	const handleLang = (ev) => {
-		if (ev.target.id) {
-			dispatch(changeLanguage(ev.target.id));
-		}
-	};
+  const handleLang = (ev) => {
+    if (ev.target.id) {
+      dispatch(changeLanguage(ev.target.id));
+    }
+  };
 
-	const handleTheme = () => {
-		dispatch(toggleTheme());
-	};
+  const handleTheme = () => {
+    dispatch(toggleTheme());
+  };
 
-	return (
-		<NavbarStyled>
-			<div className='wrapper navbar__top'>
-				<div className='visits'>
-					<StyledSVG src={user} />
-
-					<p>
-						{strings[language].vis}{' '}
-						<span className='number'>{visits}</span>{' '}
-					</p>
-				</div>
-				<ul className='navbar__options'>
-					<Dropdown>
-						<Fade>
-							<StyledSVG src={languageIcon} />
-						</Fade>
-						<Fade>
-							<span>{strings[language].language}</span>
-						</Fade>
-						<ul onClick={(e) => handleLang(e)}>
-							<Fade duration={200}>
-								<li>
-									<p
-										id='en'
-										className={
-											language === 'en'
-												? 'selected'
-												: null
-										}
-									>
-										{strings[language].language_en}
-									</p>{' '}
-								</li>
-								<li>
-									<p
-										id='es'
-										className={
-											language === 'es'
-												? 'selected'
-												: null
-										}
-									>
-										{strings[language].language_es}
-									</p>
-								</li>
-							</Fade>
-						</ul>
-					</Dropdown>
-					<li>
-						<button onClick={handleTheme}>
-							<Fade>
-								<StyledSVG
-									src={theme === 'light' ? sun : moon}
-								/>
-							</Fade>
-							<Fade>
-								<span>{strings[language].theme}</span>
-							</Fade>
-						</button>
-					</li>
-				</ul>
-			</div>
-		</NavbarStyled>
-	);
+  return (
+    <NavbarStyled>
+      <div className="wrapper navbar__top">
+        <ul className="navbar__options">
+          <Dropdown>
+            <Fade>
+              <StyledSVG src={languageIcon} />
+            </Fade>
+            <Fade>
+              <span>{strings[language].language}</span>
+            </Fade>
+            <ul onClick={(e) => handleLang(e)}>
+              <Fade duration={200}>
+                <li>
+                  <p id="en" className={language === "en" ? "selected" : null}>
+                    {strings[language].language_en}
+                  </p>{" "}
+                </li>
+                <li>
+                  <p id="es" className={language === "es" ? "selected" : null}>
+                    {strings[language].language_es}
+                  </p>
+                </li>
+              </Fade>
+            </ul>
+          </Dropdown>
+          <li>
+            <button onClick={handleTheme}>
+              <Fade>
+                <StyledSVG src={theme === "light" ? sun : moon} />
+              </Fade>
+              <Fade>
+                <span>{strings[language].theme}</span>
+              </Fade>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </NavbarStyled>
+  );
 };
 
 export default Navbar;
